2f50a9a02e897d5cb66ff028aaf9bb429e91d3c5
kosukehomma <kou4321ske@gmail.com>
2022-07-15 14:00:38 +0900

    implement component using mltiple states

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment2 = () => setCount((previousCount) => previousCount + 1)
-  const decrement2 = () => setCount((previousCount) => previousCount - 1)
-
-  const reset = () => setCount(0);
-
-  const cross = () => setCount(previousCount => previousCount * 2);
-
-  const threeCut = () => setCount(count => count % 3 === 0 ? count / 3 : count)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={cross}>x2</button>
-        <button onClick={threeCut}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Rset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
