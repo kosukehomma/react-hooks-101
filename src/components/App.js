1e6b18bf943880e01da3966f241408b8a04f347b
kosukehomma <kou4321ske@gmail.com>
2022-07-18 10:45:06 +0900

    implement all events deletion feature

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = () => {
@@ -22,6 +23,13 @@ const App = () => {
     setBody('')
   }
 
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
+    if(result) dispatch({ type: 'DELETE_ALL_EVENTS' })
+  }
+
+  const unCreatable = title === '' || body === ''
   
   return (
     <div className="container-fluid">
@@ -37,8 +45,8 @@ const App = () => {
           <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
       </form>
 
       <h4>イベント一覧</h4>
@@ -52,7 +60,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-
+          { state.map((event, index)=> (<Event key={index} event={event} dispatch={dispatch}/>)) }
         </tbody>
       </table>
     </div>
